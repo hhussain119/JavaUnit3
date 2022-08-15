8dce3c57202e0543de42b44c54672413b6db6734
zyditt <admin@zyditt.com>
2022-08-15 02:07:17 -0400

    setup for part 3

--- src/main/java/com/javaunit3/springmvc/SpringmvcApplication.java
+++ src/main/java/com/javaunit3/springmvc/SpringmvcApplication.java
@@ -1,5 +1,9 @@
 package com.javaunit3.springmvc;
 
+import com.javaunit3.springmvc.model.MovieEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(MovieEntity.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
 		SpringApplication.run(SpringmvcApplication.class, args);
+		session.close();
 	}
 
 }
